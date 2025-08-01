c23f1c36c12d64ec136ad2bd854e1cd8315fdffa
Christopher Guaña <132909268+Christopher9653@users.noreply.github.com>
2025-08-01 12:46:44 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -6,10 +6,11 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'empleados_crud'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
